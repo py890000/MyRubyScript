bd764923a9e52e83bf039d4612459ef15487c7e3
py890000 <py890000@gmail.com>
2013-03-20 16:49:33 +0800

    Update script.rb
    ddd

--- script.rb
+++ script.rb
@@ -11,13 +11,13 @@ excel = WIN32OLE.new("excel.application")
 workbook = excel.Workbooks.Open('E:\railsWork\myStudy\sql_result_4000.xls')
 worksheet = workbook.Worksheets(1)
 #worksheet.Select
-filex = File.new('updateHitaosql3.sql', File::CREAT|File::TRUNC|File::RDWR, 0644)
+filex = File.new('updatesql3.sql', File::CREAT|File::TRUNC|File::RDWR, 0644)
 for i in 2..3001 do
 	if !worksheet.Range('d'+i.to_s).value
    next
   end
 	
-  filex.write("update hitao_order_coupons set mobile='")
+  filex.write("update _order_coupons set mobile='")
   filex.write(getSheetByIndex(worksheet.Range('d'+i.to_s).value.to_s))
   filex.write("' WHERE source_id  =")
   filex.write(worksheet.Range('a'+i.to_s).value)  
